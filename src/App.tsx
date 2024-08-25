c690f0d0ae350d859c04b8d1e3f32e699bda1897
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-08-24 23:24:20 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import useGetReservations from './hooks/useGetReservations'
+import { Reservation } from './types/reservationsTypes'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data, error, isLoading } = useGetReservations()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error: {error.message}</div>
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {data?.data.map((reservation : Reservation) => (
+          <div key={reservation._id}>
+            <p>{reservation.name}</p>
+            <p>{reservation.email}</p>
+            <p>{reservation.phone}</p>
+            <p>{reservation.numberOfPersons}</p>
+            <p>{reservation.tourName}</p>
+            <p>{reservation.dateStartingTour}</p>
+            <p>{reservation.status}</p>
+            <p>{reservation.orderNumber}</p>
+          </div>
+        ))}
       </p>
     </>
   )
