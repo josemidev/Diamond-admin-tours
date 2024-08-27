236f6c451842e315a6b33967e3a2afde4f427e1a
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-27 00:53:25 -0500

    change:: update input form
    Able to render as an Input type Password or Text

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -3,14 +3,19 @@ import { Form, Input, type InputProps } from "antd";
 interface InputFormProps extends InputProps {
   formName: string;
   label?: string;
+  type?: 'text' | 'password';
 }
 
-export default function InputForm({ formName, label, ...props }: InputFormProps) {
+export default function InputForm({ formName, label, type, ...props }: InputFormProps) {
   return (
     <section className="flex flex-col gap-1">
       {label && <p className="text-[14px] text-[#000000] ml-1 text-start">{label} *</p>}
       <Form.Item name={formName} noStyle required>
-        <Input placeholder="Type here" {...props} />
+        {type === 'password' ?
+          <Input.Password placeholder="Type here" {...props} />
+          :
+          <Input placeholder="Type here" {...props} />
+        }
       </Form.Item>
     </section>
   );
