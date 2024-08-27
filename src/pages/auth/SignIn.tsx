c6830ad69a6a84c95e8438fe0950be89ccc95b5e
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-27 00:30:14 -0500

    change:: update sign in page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,8 +1,15 @@
+import FormSignIn from "../../UI/FormSignIn";
 
-function SignIn() {
+export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className="flex h-screen">
+      <div className="w-9/12 hidden md:!block">
+        <div className='!h-full !bg-[url(/images/SignIn.webp)] bg-center bg-no-repeat'>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 bg-[url(/images/SignIn.webp)] bg-cover bg-no-repeat md:bg-white md:!bg-none grid h-screen">
+        <FormSignIn />
+      </div>
+    </div>
   )
 }
-
-export default SignIn
