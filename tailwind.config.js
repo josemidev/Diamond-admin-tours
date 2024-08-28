3c4cfb863a6f9f353e32eaca80580230a1a59022
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-28 00:30:16 -0500

    add:: add more colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       colors: {
         diamondPrimary: "#3655A0",
         diamondGray: "#D6D6D6",
+        diamondBlack1: "#212121E0",
+        diamondBlack2: "#212121C7",
+        disabled: "#C1C1C1",
       },
     },
   },
