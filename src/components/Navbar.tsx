e205369e721b30cd1c7fb3b3e2504c37db349c90
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-08-28 23:11:21 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import Logo from '../assets/svg/logoColors.svg'
-import { Avatar } from "antd"
 import { UserOutlined } from "@ant-design/icons"
+import { Avatar } from "antd"
+import Logo from '../assets/svg/logoColors.svg'
 
 export default function Navbar() {
   return (
