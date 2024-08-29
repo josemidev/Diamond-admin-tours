121827989cec68807a66a436d0dd1343fef3a61f
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-28 21:38:20 -0500

    change:: adapt dashboard only to desktop screens

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,8 +4,15 @@ import Sidebar from "../../components/Sidebar"
 function index() {
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <section className="hidden lg:block">
+        <Navbar />
+        <Sidebar />
+      </section>
+      <section className="flex justify-center items-center h-screen lg:hidden">
+        <p className="text-diamondBlack1 font-medium max-w-xs text-center leading-5">
+          Dashboard disponible solo en pantallas grandes
+        </p>
+      </section>
     </>
   )
 }
