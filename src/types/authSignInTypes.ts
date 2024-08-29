7bdcfd0d6d90dd6faf7f04868f35f6fe59673fd9
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-08-29 00:37:01 -0500

    Create authSignInTypes.ts

---
+++ src/types/authSignInTypes.ts
@@ -0,0 +1,21 @@
+export interface SignInResponse {
+  statusCode: number
+  message: string
+  data: Data
+}
+
+export interface SignInRequest {
+  username: string
+  password: string
+}
+
+  interface Data {
+  user: User
+  access_token: string
+}
+
+  interface User {
+  name: string
+  username: string
+  roles: string[]
+}
