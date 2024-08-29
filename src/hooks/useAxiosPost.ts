3978b41902f5323864ca1ef7972e4e24917ae0cc
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-08-28 23:11:24 -0500

    Update useAxiosPost.ts

--- src/hooks/useAxiosPost.ts
+++ src/hooks/useAxiosPost.ts
@@ -8,12 +8,10 @@ const HOST: string = "https://tours-be.fly.dev/api/v1"
 function useAxiosPost<TData, TParams extends any[]> (
   pathname: string,
   config: Options<TData, TParams>
-) {
+): Result<TData, TParams> {
   const { cancel, data, error, loading, mutate, params, refresh, refreshAsync, run, runAsync } =
-    useRequest<TData, TParams>(async () => {
-      const response = await axios.get<TData>(`${HOST}${pathname}`);
-      return response.data;
-    }, {
+    useRequest(async (body): Promise<TData> => await axios.post(`${HOST}${pathname}`, body, {}), {
+      manual: true,
       ...config
     })
 
