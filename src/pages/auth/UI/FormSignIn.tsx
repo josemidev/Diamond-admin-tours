96facc35bd754bbb0cc3f4c2de3b435887155000
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-08-28 23:11:35 -0500

    Update FormSignIn.tsx

--- src/pages/auth/UI/FormSignIn.tsx
+++ src/pages/auth/UI/FormSignIn.tsx
@@ -1,12 +1,36 @@
 import { Form } from "antd";
 
 import InputForm from "@/components/InputForm";
+import useAxiosPost from "@/hooks/useAxiosPost";
 import { MailFilled } from "@ant-design/icons";
 
 export default function FormSignIn() {
-  const onFinish = (values: any) => {
-    console.log(values);
+
+  const action = useAxiosPost('/auth/login', {
+    manual: true,
+    onError: (e) => {
+      const mgs = e
+      console.log(mgs)
+      // AlertError(mgs)
+    },
+    onSuccess: (data: unknown) => {
+      const mgs = data
+      console.log(mgs)
+      // refresh()
+      // AlertSuccess('la billetera se ha registrado exitosamente')
+    }
+  })
+
+  function onFinish(values: { username: string, password: string }): void {
+    const { username, password } = values;
+    action.run({
+      username,
+      password
+    })
   }
+
+  // user admin
+  //  contraseña pyh*4Vw2
 
   return (
     <>
@@ -21,7 +45,7 @@ export default function FormSignIn() {
           <section className="mt-6 flex flex-col gap-[22px]">
             <InputForm
               label="Email"
-              formName="email"
+              formName="username"
               prefix={<MailFilled style={{ color: '#9D9D9D' }} />}
             />
             <InputForm
