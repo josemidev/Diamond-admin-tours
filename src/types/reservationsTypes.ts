a12b8869e9b9a02f7e580107b2dc7fb392968f5b
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-28 21:36:38 -0500

    change:: update types

--- src/types/reservationsTypes.ts
+++ src/types/reservationsTypes.ts
@@ -1,12 +1,18 @@
 export interface Reservation {
-  _id: string
-  name: string
-  email: string
-  phone: string
-  numberOfPersons: number
-  tourName: string
-  dateStartingTour: string
-  status: string
-  orderNumber: string
-  changeHistory: unknown[]
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  numberOfPersons: number;
+  tourName: string;
+  dateStartingTour: string;
+  status: string;
+  orderNumber: string;
+  changeHistory: unknown[];
+}
+
+export interface IReservationCardProps {
+  //Details
+  item?: string;
+  content?: string;
 }
