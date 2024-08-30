f10cff0464182454d2326161d7194994b69b3f21
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-30 00:37:55 -0500

    change:: update reservation types file
    - Add data to select name tour
    - Add props to cards

--- src/types/reservationsTypes.ts
+++ src/types/reservationsTypes.ts
@@ -13,6 +13,8 @@ export interface Reservation {
 
 export interface IReservationCardProps {
   data?: Reservation;
+  isSearching?: boolean;
+  sx?: string;
   //Details
   item?: string;
   content?: string;
@@ -26,3 +28,50 @@ export interface IReservationDrawerProps {
 export interface IStatusCardProps {
   status: "unrevised" | "review" | "approved" | "rejected";
 }
+
+export const Tours = [
+  {
+    label: "Coco Bongo",
+    value: "COCO_BONGO",
+  },
+  {
+    label: "Isla Saona",
+    value: "ISLA_SAONA",
+  },
+  {
+    label: "Punta Cana Tour Buggy",
+    value: "PUNTA_CANA_TOUR_BUGGY",
+  },
+  {
+    label: "Monkeyland Tour",
+    value: "MONKEYLAND_TOUR",
+  },
+  {
+    label: "Supreme Safari Tour",
+    value: "SUPREME_SAFARI_TOUR",
+  },
+  {
+    label: "Punta Cana Tour Higuey",
+    value: "PUNTA_CANA_TOUR_HIGUEY",
+  },
+  {
+    label: "Punta Cana Tour Santo Domingo",
+    value: "PUNTA_CANA_TOUR_SANTO_DOMINGO",
+  },
+  {
+    label: "Tour Horse",
+    value: "TOUR_HORSE",
+  },
+  {
+    label: "Punta Cana Tour Zip Line Park",
+    value: "PUNTA_CANA_TOUR_ZIP_LINE_PARK",
+  },
+  {
+    label: "La Hacienda Park",
+    value: "LA_HACIENDA_PARK",
+  },
+  {
+    label: "Punta Cana Tour Party Boat",
+    value: "PARTY_BOAT_TOUR",
+  },
+];
