a40779f712be08df85b9e0d713c4d2ded87c3b1f
samuelrdg11 <samuelrdg11@gmail.com>
2024-08-30 23:01:36 -0500

    add:: axios put hook

---
+++ src/hooks/useAxiosPut.ts
@@ -0,0 +1,46 @@
+import { useRequest } from "ahooks";
+import type { Options, Result } from "ahooks/lib/useRequest/src/types";
+import axios from "../api/tours";
+
+const HOST: string = "https://tours-be.fly.dev/api/v1";
+
+function useAxiosPut<TData, TParams extends [body: unknown]>(
+  pathname: string,
+  config: Options<TData, TParams>
+): Result<TData, TParams> {
+  const {
+    cancel,
+    data,
+    error,
+    loading,
+    mutate,
+    params,
+    refresh,
+    refreshAsync,
+    run,
+    runAsync,
+  } = useRequest(
+    async (body: TParams[0]): Promise<TData> => {
+      return await axios.put(`${HOST}${pathname}`, body, {});
+    },
+    {
+      manual: true,
+      ...config,
+    }
+  );
+
+  return {
+    loading,
+    data,
+    error,
+    params,
+    run,
+    runAsync,
+    refresh,
+    refreshAsync,
+    mutate,
+    cancel,
+  } as Result<TData, TParams>;
+}
+
+export default useAxiosPut;
