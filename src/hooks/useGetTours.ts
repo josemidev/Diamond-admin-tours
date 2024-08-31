290d25296fb8ffe626635e276c9509298a1d15ad
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-08-30 23:00:39 -0500

    Update useGetTours.ts

--- src/hooks/useGetTours.ts
+++ src/hooks/useGetTours.ts
@@ -1,11 +1,11 @@
+import { type ToursResponse } from "@/types/toursType"
 import { useQuery } from "@tanstack/react-query"
 import api from "../api/tours"
-import { Reservation } from "../types/reservationsTypes"
 
 const HOST: string = "https://tours-be.fly.dev/api/v1"
 
 async function getTours() {
-  const { data } = await api.get<Reservation[]>(`${HOST}/dashboard/tours`)
+  const { data } = await api.get<ToursResponse[]>(`${HOST}/dashboard/tours`)
   return data
 }
 
