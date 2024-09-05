7f6c5b579977929d40f2f81c4a46f4350160629a
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-09-04 19:10:35 -0500

    Update SignIn.tsx

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -4,7 +4,7 @@ export default function SignIn() {
   return (
     <div className="flex h-screen">
       <div className="w-10/12 hidden lg:!block">
-        <div className='!h-full !bg-[url(/images/SignIn.webp)] bg-center bg-no-repeat'>
+        <div className='!h-full !bg-[url(/images/SignIn.webp)] bg-center bg-no-repeat bg-cover'>
         </div>
       </div>
       <div className="w-full lg:w-1/2 bg-[url(/images/SignIn.webp)] bg-cover bg-no-repeat lg:bg-white lg:!bg-none px-5 lg:px-0 grid">
