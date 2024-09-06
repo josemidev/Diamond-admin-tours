333fcf5091c88483caa13fe56484dc34fd005d32
josemi.perezsolano <josemi.perezsolano@gmail.com>
2024-09-05 20:24:51 -0500

    update vite-plugin-verce

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   resolve: {
     alias: {
       "@": "/src",
