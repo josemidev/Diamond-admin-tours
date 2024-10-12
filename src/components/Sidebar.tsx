bdc6f94fe0f294e7cc70d15fbe69d2fb05a5951d
samuelrdg11 <samuelrdg11@gmail.com>
2024-10-11 20:14:49 -0500

    change:: update sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,23 @@
+import useGetCurrentUser from "@/hooks/useGetCurrentUser"
 import AllReservation from '@/pages/home/UI/All'
 import AllReservationArchived from '@/pages/home/UI/AllArchived'
 import Clients from "@/pages/home/UI/Clients"
 import SearchReservations from '@/pages/home/UI/SearchReservations'
 import Tours from "@/pages/home/UI/Tours"
 import Users from '@/pages/home/UI/Users'
-/* import Users from "@/pages/home/UI/Users" */
+import { ICurrentUserProps } from "@/types/userTypes"
 import { Tabs, TabsProps, Tooltip } from "antd"
 import { useState } from 'react'
+import LoadingIndicator from "./LoadingIndicator"
+import MyReservations from "@/pages/home/UI/MyReservations"
 
 export default function Sidebar() {
+  const { data, isLoading } = useGetCurrentUser()
+  const currentUser: ICurrentUserProps = data || {}
+  const isOwnerOrAdmin = currentUser.roles?.includes('owner' || 'admin')
   const tab = localStorage.getItem('tab')
   const [TAB, setTAB] = useState(tab || 'all')
+
   const itemsTab: TabsProps['items'] = [
     {
       key: 'all',
@@ -37,11 +44,16 @@ export default function Sidebar() {
       label: <p className={`${TAB === 'tours' ? 'font-bold text-diamondPrimary' : 'font-normal text-diamondBlack2'}`}>Tours</p>,
       children: <Tours />
     },
-      {
-          key: 'users',
-          label: <p className={`${TAB === 'users' ? 'font-bold text-diamondPrimary' : 'font-normal text-diamondBlack2'}`}>Usuarios</p>,
-          children: <Users />
-        },
+    {
+      key: 'users',
+      label: <p className={`${TAB === 'users' ? 'font-bold text-diamondPrimary' : 'font-normal text-diamondBlack2'}`}>Usuarios</p>,
+      children: <Users />
+    },
+    {
+      key: 'my_reservations',
+      label: <p className={`${TAB === 'my_reservations' ? 'font-bold text-diamondPrimary' : 'font-normal text-diamondBlack2'}`}>Mis Reservas</p>,
+      children: <MyReservations />
+    },
     {
       key: 'metrics',
       disabled: true,
@@ -53,22 +65,23 @@ export default function Sidebar() {
         </Tooltip>,
       children: <p>Métricas</p>
     },
-    
   ]
 
   return (
-    <Tabs
-      tabBarGutter={10}
-      tabPosition="left"
-      defaultActiveKey={TAB}
-      activeKey={TAB}
-      items={itemsTab}
-      onChange={(key) => {
-        setTAB(key)
-        localStorage.setItem('tab', key)
-      }}
-      tabBarStyle={{ marginTop: 24, position: 'sticky', top: 0, left: 0, width: 180, overflowY: 'auto' }}
-      style={{ overflowY: 'auto', height: '100vh' }}
-    />
+    isLoading ?
+      <LoadingIndicator /> :
+      <Tabs
+        tabBarGutter={10}
+        tabPosition="left"
+        defaultActiveKey={TAB}
+        activeKey={TAB}
+        items={itemsTab}
+        onChange={(key) => {
+          setTAB(key)
+          localStorage.setItem('tab', key)
+        }}
+        tabBarStyle={{ marginTop: 24, position: 'sticky', top: 0, left: 0, width: 180, overflowY: 'auto' }}
+        style={{ overflowY: 'auto', height: '100vh' }}
+      />
   )
 }
