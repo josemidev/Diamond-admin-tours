c391ab273673a2502a150b3eb2eb5edf3b549467
samuelrdg11 <samuelrdg11@gmail.com>
2024-10-16 21:06:34 -0500

    change:: update input component to be reusable

--- src/components/FormElements/InputForm.tsx
+++ src/components/FormElements/InputForm.tsx
@@ -3,20 +3,21 @@ import { Form, Input, type InputProps } from "antd";
 interface InputFormProps extends InputProps {
   formName: string;
   label?: string;
-  type?: 'text' | 'password' | 'email';
+  type: 'text' | 'password' | 'number';
   required?: boolean;
 }
 
 export default function InputForm({ formName, label, type, required = true, ...props }: InputFormProps) {
+  const typeOfInput = {
+    text: <Input placeholder="Escribe aquí" {...props} />,
+    password: <Input.Password placeholder="Escribe aquí" {...props} />,
+    number: <Input type='number' min={50} placeholder="Escribe aquí" {...props} />,
+  }
   return (
     <section className="flex flex-col gap-1">
       {label && <p className="text-[14px] text-[#000000] ml-1 text-start">{label} *</p>}
       <Form.Item name={formName} noStyle rules={[{ required: required, message: 'Please fill this field' }]}>
-        {type === 'password' ?
-          <Input.Password placeholder="Escribe aquí" {...props} />
-          :
-          <Input placeholder="Escribe aquí" {...props} />
-        }
+        {typeOfInput[type || 'text'] || <Input placeholder="Escribe aquí" {...props} />}
       </Form.Item>
     </section>
   );
