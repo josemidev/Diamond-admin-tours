d5def16087d7e54cf12f00541d5fdf04cf987a93
Carlos Maza <mazagonzalez61@gmail.com>
2025-05-05 01:20:19 -0500

    feat:: progress on my transfer

--- src/components/Transfer/Transfer.tsx
+++ src/components/Transfer/Transfer.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, Select, theme, Steps, message } from "antd";
+import { Form, Input, Button, Select, theme, Steps, message, TimePicker, Radio, RadioChangeEvent } from "antd";
+import DatePickerForm from '../FormElements/DatePickerForm';
 
 type TransferProps = {
     modal: boolean;
@@ -14,31 +15,30 @@ export default function Transfer({ setModal, value }: TransferProps): JSX.Elemen
 
     const { token } = theme.useToken();
     const [current, setCurrent] = useState(0);
+    const [valuePay, setValuePay] = useState(0);
 
     const next = () => {
         setCurrent(current + 1);
     };
 
     const steps = [
-        {
-            key: 1,
-            title: '',
-        },
-        {
-            key: 2,
-            title: '',
-        },
-        {
-            key: 3,
-            title: '',
-        },
-        {
-            key: 4,
-            title: '',
-        },
+        { key: 1, title: '' },
+        { key: 2, title: '' },
+        { key: 3, title: '' },
+        { key: 4, title: '' },
     ];
 
     const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+    const style: React.CSSProperties = {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 9,
+    };
+
+    const onChange = (e: RadioChangeEvent) => {
+        setValuePay(e.target.value);
+    };
     
     return (
         <>
@@ -49,7 +49,7 @@ export default function Transfer({ setModal, value }: TransferProps): JSX.Elemen
             >
                 {steps[current].key === 1 &&(
                     <section>
-                        <section className="space-y-4 my-7">
+                        <div className="space-y-4 my-7">
                             <p className="text-diamondBlack3 capitalize text-[24px] font-bold">
                                 {value === 1 && (
                                     <span>Registrar Transfer</span>
@@ -64,7 +64,7 @@ export default function Transfer({ setModal, value }: TransferProps): JSX.Elemen
                                 )}
                             </p>
                             <span className="text-[#646464] font-light text-sm">Completa el formulario con los datos del transfer y el cliente.</span>
-                        </section>
+                        </div>
                         <div className="grid grid-cols-2 gap-3 mt-4">
                             {(value === 1 || value === 3) && (
                                 <Input type="text" placeholder="Lugar de recogida" />
@@ -89,20 +89,20 @@ export default function Transfer({ setModal, value }: TransferProps): JSX.Elemen
                         </div>
 
                         <div className="grid grid-cols-2 gap-3 mt-4">
-                            <Input type="date" />
-                            <Input type="time" />
+                            <DatePickerForm formName='dateStartingTour' placeholder='Fecha' />
+                            <TimePicker placeholder='Hora' />
                         </div>
                     </section>
                 )}
 
                 {steps[current].key === 2 &&(
                     <section>
-                        <section className="space-y-4 my-7">
+                        <div className="space-y-4 my-7">
                             <p className="text-diamondBlack3 capitalize text-[24px] font-bold">
                                 Seleccionar Vehículo
                             </p>
                             <span className="text-[#646464] font-light text-sm">Completa el formulario con los datos del transfer y el cliente.</span>
-                        </section>
+                        </div>
                         <div className='grid grid-cols-1 mb-10'>
                             <Select
                                 showSearch
@@ -120,22 +120,51 @@ export default function Transfer({ setModal, value }: TransferProps): JSX.Elemen
 
                 {steps[current].key === 3 &&(
                     <section>
-                        <section className="mt-3 mb-5 space-y-4">
+                        <div className="mt-3 mb-5 space-y-4">
                             <p className="text-diamondBlack3 capitalize text-[24px] font-bold">
                                 Detalles del Cliente
                             </p>
                             <span className="text-[#646464] font-light text-sm">Completa el formulario con los datos del transfer y el cliente.</span>
-                        </section>
+                        </div>
+                        <div className='grid grid-cols-2 gap-x-3 gap-y-4'>
+                            <Input placeholder='Número de personas' />
+                            <Input placeholder='Número de Orden' />
+                            <Input placeholder='# de Maletas' />
+                            <Input placeholder='Número de Vuelo del Cliente' />
+                            <Input placeholder='Nombre' />
+                            <Input placeholder='Apellido' />
+                            <Input placeholder='Teléfono' />
+                            <Input placeholder='Email' />
+                        </div>
+                        <Input type='' placeholder='Información Adicional, ej: habitación de hotel.' className='min-h-[85px] mt-4 text-start' />
                     </section>
                 )}
 
                 {steps[current].key === 4 &&(
                     <section>
-                        <section className="my-7">
+                        <div className="my-7">
                             <p className="text-diamondBlack3 capitalize text-[24px] font-bold">
                                 Opciones de Pago
                             </p>
-                        </section>
+                        </div>
+                        <div>
+                            <div className='mb-7'>
+                                <label className='text-[#646464] text-sm'>Monto a Cobrar</label>
+                                <Input value={'$' + 90000} />
+                            </div>
+
+                            <Radio.Group 
+                                 style={style} 
+                                 onChange={onChange}
+                                 value={valuePay}
+                                 options={[
+                                     {value: 1, label: 'Paypal'},
+                                     {value: 2, label: 'Zelle'},
+                                     {value: 3, label: 'Pagar Después'},
+                                     {value: 4, label: 'Efectivo'},
+                                 ]}
+                            />
+                        </div>
                     </section>
                 )}
 
